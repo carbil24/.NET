a9faba797bcf11cdcb1070eb95f90694921da224
carbil24 <carbil2@hotmail.com>
2020-01-24 18:12:25 -0500

    ASP.NET

---
+++ ASP.NET_Test1/ASP.NET_Test1/RequestObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace ASP.NET_Test1
+{
+    public class RequestObject
+    {
+        private HttpRequest _request;
+
+        public RequestObject(HttpRequest request)
+        {
+            _request = request;
+        }
+
+        public string browserInfoFromRegularClass()
+        {
+            StringBuilder browserInfo = new StringBuilder();
+
+            browserInfo.Append("<br/>Browser Info from Regular Class:<br/>");
+
+            browserInfo.Append($"Browser : {_request.Browser.Browser}<br/>");
+            browserInfo.Append($"Browser Version : {_request.Browser.Version}<br/>");
+            browserInfo.Append($"Client's Platform : {_request.Browser.Platform}<br/>");
+            browserInfo.Append($".NET CLR Version : {_request.Browser.ClrVersion}<br/>");
+            browserInfo.Append($"ECMA Script Version : {_request.Browser.EcmaScriptVersion}<br/>");
+            browserInfo.Append($"MS Html Document Object Model Version : {_request.Browser.MSDomVersion}<br/>");
+            browserInfo.Append($"W3C XML Document and Object Model Version : {_request.Browser.W3CDomVersion}<br/>");
+
+            return browserInfo.ToString();
+        }
+    }
+}
---
+++ ASP.NET_Test1/ASP.NET_Test1/Test.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.NET_Test1
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            StringBuilder browserInfo = new StringBuilder();
+
+            browserInfo.Append("Browser Info from Page Class:<br/>");
+            browserInfo.Append($"Browser : {Request.Browser.Browser}<br/>");
+            browserInfo.Append($"Browser Version : {Request.Browser.Version}<br/>");
+            browserInfo.Append($"Client's Platform : {Request.Browser.Platform}<br/>");
+            browserInfo.Append($".NET CLR Version : {Request.Browser.ClrVersion}<br/>");
+            browserInfo.Append($"ECMA Script Version : {Request.Browser.EcmaScriptVersion}<br/>");
+            browserInfo.Append($"MS Html Document Object Model Version : {Request.Browser.MSDomVersion}<br/>");
+            browserInfo.Append($"W3C XML Document and Object Model Version : {Request.Browser.W3CDomVersion}<br/>");
+
+            Response.Write(browserInfo.ToString());
+
+            //Calling the regular class
+            RequestObject req = new RequestObject(Request);
+
+            string browserInfoFromRegularClass = req.browserInfoFromRegularClass();
+
+            Response.Write(browserInfoFromRegularClass); 
+        }
+    }
+}
