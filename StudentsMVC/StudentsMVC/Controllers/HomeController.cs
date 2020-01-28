8fb8c993633062e26c5c5cf9af2a407e824a427c
carbil24 <carbil2@hotmail.com>
2020-01-27 19:55:12 -0500

    ASP.NET MVC

---
+++ StudentsMVC/StudentsMVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentsMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult HelloWorld(string name)
+        {
+            ViewBag.Message = "Hello from HelloWorld Action on Home Controller";
+
+            Session["name"] = name;
+            ViewBag.MyName = name + " from ViewBag";
+
+            return View("HelloWorldWebForm");
+        }
+    }
+}
